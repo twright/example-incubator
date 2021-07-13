a89a43189c87f65741343cf59c642c147b80316b
Claudio Gomes <claudio.gomes@eng.au.dk>
2021-07-13 10:49:40 +0200

    Fix failing script.

--- communication/installation/test_server.py
+++ communication/installation/test_server.py
@@ -2,15 +2,19 @@ import logging
 import time
 
 from communication.server.rabbitmq import Rabbitmq
+from config.config import config_logger, load_config
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.ERROR)
 
-    receiver = Rabbitmq(ip="localhost")
+    config_logger("logging.conf")
+    config = load_config("startup.conf")
+
+    receiver = Rabbitmq(**config)
     receiver.connect_to_server()
     qname = receiver.declare_local_queue(routing_key="test")
 
-    sender = Rabbitmq(ip="localhost")
+    sender = Rabbitmq(**config)
     sender.connect_to_server()
     sender.send_message(routing_key="test", message={"text": "321"})
 
